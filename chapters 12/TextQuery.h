d7af2b9b5104c5a35cc08e92d8abc5112111ddf3
ZhangChuanzhi <zhang_chuanzhi@outlook.com>
2017-11-12 11:48:04 +0800

    TextQuery
    shared_ptr<vector<string>> version.
    no function implementations.

---
+++ chapters 12/TextQuery.h
@@ -0,0 +1,24 @@
+#ifndef TEXTQUERY_H
+#define TEXTQUERY_H
+ 
+#include <fstream>
+#include<memory> 
+#include<vector>
+#include<string>
+#include<set>
+#include<map>
+
+class QueryResult;
+class TextQuery{
+	public:
+		using line_no = std::vector<std::string>::size_type;//为什么放在public里面 
+		TextQuery(std::ifstream&);//构造函数
+		QueryResult query(const std::string&) const; 
+	private:
+		std::shared_ptr<std::vector<std::string>> file;//下标 到 一行文本的映射 
+		std::map<std::string,
+			std::shared_ptr<std::set<line_no>>> wm;//单词 到 Set的映射 
+
+};
+
+#endif 
